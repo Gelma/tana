0fba1a39e1a687c97c971e1fa74c873029741c1e
endeios@gmail.com <endeios@gmail.com>
2015-03-04 16:03:50 +0100

    aggiunti template base per gestione creazione prenotazioni

--- aeroplanisti/aeroplanisti/urls.py
+++ aeroplanisti/aeroplanisti/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'aeroplanisti.views.home', name='home'),
-    url(r'^login/$', 'aeroplanisti.views.login', name='login'),
-    url(r'^logout/$', 'aeroplanisti.views.logout', name='logout'),
+#    url(r'^login/$', 'aeroplanisti.views.login', name='login'),
+#    url(r'^logout/$', 'aeroplanisti.views.logout', name='logout'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- aeroplanisti/aeroplanisti/views.py
+++ aeroplanisti/aeroplanisti/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
+from datetime import datetime
 from django.contrib.auth import authenticate, login
+from bookings.models import Booking
 # Create your views here.
 
 def home(request):
     '''the home request, should show app home page'''
-    return render(request,'home.html');
+    if request.method=='GET':
+        today = datetime.now()
+        bookings = Booking.objects.all()
+        return render(request,'home.html',{'today':today,'bookings':bookings});
+    if request.method =='POST':
+        pass
+    return redirect('home')
 
 def login(request):
     '''the login procedure'''
